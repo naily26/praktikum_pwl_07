27d400ca08fcc3889ece22695836b08396b05608
naily26 <nailymasruri@gmail.com>
2021-03-25 19:41:50 +0700

    buat migration

---
+++ database/migrations/2021_03_25_101031_create_mahasiswas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->string('nim', 10)->primary();
+            $table->string('nama', 50)->nullabel();
+            $table->string('kelas', 10)->nullabel();
+            $table->string('jurusan', 30)->nullabel();
+            $table->string('no_handphone', 15)->nullabel();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+}
