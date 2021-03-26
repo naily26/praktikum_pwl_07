412ff5f3b3be7eb937aeefdb4b0e4c7fdc6ed2f0
naily26 <nailymasruri@gmail.com>
2021-03-26 21:27:16 +0700

    update tabel mahasiswa

---
+++ database/migrations/2021_03_26_141354_update_table_mahasiswa.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateTableMahasiswa extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mahasiswa', function (Blueprint $table) {
+            $table->string('email', 500)->after('nama')->unique()->nullable();
+            $table->date('tanggal_lahir')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+}
