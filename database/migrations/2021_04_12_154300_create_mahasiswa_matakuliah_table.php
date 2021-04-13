1587a5042d6e75b0d06f7b2d25a4c85c10158f7e
naily26 <nailymasruri@gmail.com>
2021-04-13 10:41:03 +0700

    membuat migration table mahasiswa_matakuliah

--- database/migrations/2021_04_12_154300_create_mahasiswa_matakuliah_table.php
+++ database/migrations/2021_04_12_154300_create_mahasiswa_matakuliah_table.php
@@ -15,6 +15,11 @@ class CreateMahasiswaMatakuliahTable extends Migration
     {
         Schema::create('mahasiswa_matakuliah', function (Blueprint $table) {
             $table->id();
+            $table->string('mahasiswa_nim')->nullable();
+            $table->foreign('mahasiswa_nim')->references('nim')->on('mahasiswa');
+            $table->unsignedBigInteger('matakuliah_id')->nullable();
+            $table->foreign('matakuliah_id')->references('id')->on('matakuliah');
+            $table->integer('nilai')->nullable();
             $table->timestamps();
         });
     }
